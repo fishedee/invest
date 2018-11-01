c1508fd9649b134724db6434ba6311d2366ede3e
fishedee <306766045@qq.com>
2018-11-02 07:30:22 +0800

    加入更多的价值指数

--- index/china_index_scene/compare.py
+++ index/china_index_scene/compare.py
@@ -4,20 +4,27 @@ import source.data as data
 import mystat.compare as compare
 
 files = [
-	['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/中证红利全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50_交易特征_时间序列分析.xls','../data/china_index/上证50全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/基本面50全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/上证红利全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/中证红利全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50_交易特征_时间序列分析.xls','../data/china_index/中证500全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/中证500全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300低波全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300价值全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300成长全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_index/中证500低波全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_index/中证500价值全收益_交易特征_时间序列分析.xls'],
-	['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_index/中证500成长全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/中证红利全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50_交易特征_时间序列分析.xls','../data/china_index/上证50全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/中证红利全收益_交易特征_时间序列分析.xls','../data/china_index/基本面50全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/基本面50全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/上证红利全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/中证红利全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50_交易特征_时间序列分析.xls','../data/china_index/中证500全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/中证500全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300低波全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300价值全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300成长全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_index/中证500低波全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_index/中证500价值全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_index/中证500成长全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/180价值全收益_交易特征_时间序列分析.xls'],
+	#['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_index/180成长全收益_交易特征_时间序列分析.xls'],
+	['../data/china_index/180价值全收益_交易特征_时间序列分析.xls','../data/china_index/沪深300价值全收益_交易特征_时间序列分析.xls'],
+	['../data/china_index/180价值全收益_交易特征_时间序列分析.xls','../data/china_index/中证500价值全收益_交易特征_时间序列分析.xls'],
+	['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_index/中证500价值全收益_交易特征_时间序列分析.xls'],
+
 ]
 
 for singleFile in files:
--- index/china_index_scene/value_compare.py
+++ index/china_index_scene/value_compare.py
@@ -4,7 +4,21 @@ import source.data as data
 import mystat.value_compare as value_compare
 
 files = [
-	['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/基本面50_财务特征_时间序列分析.xls']
+	#['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/基本面50_财务特征_时间序列分析.xls'],
+	#['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/沪深300_财务特征_时间序列分析.xls'],
+	#['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/中证500_财务特征_时间序列分析.xls']
+	#['../data/china_value/中证红利_财务特征_时间序列分析.xls','../data/china_value/基本面50_财务特征_时间序列分析.xls'],
+	#['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/上证红利_财务特征_时间序列分析.xls'],
+	#['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/中证红利_财务特征_时间序列分析.xls'],
+	#['../data/china_value/沪深300_财务特征_时间序列分析.xls','../data/china_value/上证红利_财务特征_时间序列分析.xls'],
+	#['../data/china_value/沪深300_财务特征_时间序列分析.xls','../data/china_value/中证红利_财务特征_时间序列分析.xls']
+	#['../data/china_value/沪深300_财务特征_时间序列分析.xls','../data/china_value/沪深300价值_财务特征_时间序列分析.xls'],
+	#['../data/china_value/沪深300_财务特征_时间序列分析.xls','../data/china_value/沪深300成长_财务特征_时间序列分析.xls'],
+	#['../data/china_value/中证500_财务特征_时间序列分析.xls','../data/china_value/中证500价值_财务特征_时间序列分析.xls'],
+	#['../data/china_value/中证500_财务特征_时间序列分析.xls','../data/china_value/中证500成长_财务特征_时间序列分析.xls'],
+	['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/180价值_财务特征_时间序列分析.xls'],
+	['../data/china_value/上证50_财务特征_时间序列分析.xls','../data/china_value/180成长_财务特征_时间序列分析.xls'],
+
 ]
 
 for singleFile in files:
