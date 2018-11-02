525f9fa0f54d57ffdd00fe44639da68af43e540d
fishedee <306766045@qq.com>
2018-11-02 22:10:23 +0800

    加入价值指数

--- index/china_fund_scene/index_compare.py
+++ index/china_fund_scene/index_compare.py
@@ -4,14 +4,10 @@ import source.data as data
 import mystat.compare as compare
 
 files = [
-	['../data/china_index/上证红利全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_510880_上证红利etf.xls'],
-	['../data/china_index/上证50全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_510050_上证50etf.xls'],
-	['../data/china_index/基本面50全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_160716_基本面50.xls'],
-	['../data/china_index/中证红利全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_090010_大成中证红利.xls'],
-	['../data/china_index/中证红利全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_161907_万家中证红利.xls'],
-	['../data/china_index/中证红利全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_100032_富国红利增强.xls'],
-	['../data/china_index/沪深300全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_510300_沪深300etf.xls'],
-	['../data/china_index/中证500全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_510500_中证500etf.xls'],
+	#['../data/china_index/基本面50全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值与行情_160716_基本面50.xls'],
+	#['../data/china_index/沪深300价值全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值_310398_申万菱信沪深300价值.xls'],
+	#['../data/china_index/沪深300价值全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值_519671_银河沪深300价值.xls'],
+	['../data/china_index/180价值全收益_交易特征_时间序列分析.xls','../data/china_fund/每日基金净值510030_华宝上证180价值etf.xls'],
 ]
 
 for singleFile in files:
